6f338530c18ec1c5c9ac0803c101f79c85eabc28
João Victor Modesto da Cruz <joao.modesto@progen.com.br>
2022-11-30 21:32:28 -0300

    CRUD-Template

--- backend/src/item/item.controller.js
+++ backend/src/item/item.controller.js
@@ -22,14 +22,55 @@ const findById = (req, res) => {
 };
 
 const create = (req, res) => {
+    const item = req.body;
+
+    if(!item || !item.name || !item.imageUrl || !item.category){
+        return res.status(400).send({message : "Dados invalidos!"})
+    }
+
+    const newItem = {};
+
+    res.status(201).send(newItem);
 
 };
 
 const update = (req, res) => {
+    const id = req.params.id;
+
+    if(!isObjectIdValid(id)){
+        return res.status(400).send({message : "ID invalido!"});
+    }
+
+    const item = req.body;
+
+    if(!item || !item.name || !item.imageUrl || !item.category){
+        return res.status(400).send({message : "Dados invalidos!"});
+    }
+
+    const updatedItem = {};
+
+    if(!updatedItem){
+        return res.send(404).send({messagem:"Item não encontrado!"});
+    }
+
+    res.send({message: "Item atualizado com sucesso!" });
 
 };
 
 const deleteById = (req, res) => {
+    const id = req.params.id;
+
+    if(!isObjectIdValid(id)){
+        return res.status(400).send({message : "ID invalido!"});
+    }
+
+    const deletedItem = {};
+
+    if(!deletedItem){
+        return res.status(400).send({message : "Item não encontrado!"});
+    }
+
+    res.send({message: "Item excluido com sucesso!" });
 
 };
 
